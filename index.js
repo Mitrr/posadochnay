0671ad480ee7bb642377bf03cf3b9c8083c1632e
Dmitry Levanov <mitrskoggson@yandex.ru>
2019-03-02 23:24:11 +0300

    блять работает наконец-то!

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const urlencodedParser = parser.urlencoded({extended:false});
 
 app.use(parser.json());
 app.use(urlencodedParser);
-app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use(express.static(__dirname));
+/*app.use('/', express.static(path.join(__dirname, 'js')));*/
 
 app.post("/contact", (req, res) => {
-    console.dir(req.body)
+    console.log('yep');
+    console.dir(req.body);
+    res.send("post");
 });
 
 app.get("/contact", (req, res) => {
    res.send("get");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+"/index.html")
 });
 
 app.listen(3000, () => console.log("server started"));
